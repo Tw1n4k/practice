a40d115d1dbb519802cacc75d5652c79a5a3a945
Tw1n4k <Krivonosnikolay@mail.ru>
2024-07-21 20:56:59 +0300

    Add files via upload

---
+++ Practice 2 modified.py
@@ -0,0 +1,6 @@
+a = 'Природа'
+print(a[0])
+print(a[-1])
+print(a[4:7])
+print(a[::-1])
+print(a[1:7:2])
---
+++ Practice 3 modified.py
@@ -0,0 +1,5 @@
+quantity_home_work = '12'
+quantity_hours = '1.5'
+course_name = 'python'
+time_for_one_task = float(quantity_hours) / float(quantity_home_work)
+print(course_name, quantity_home_work, quantity_hours, time_for_one_task)
