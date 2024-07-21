4f07affef615502457fd58c334fd95ae4acf2c78
Tw1n4k <Krivonosnikolay@mail.ru>
2024-07-21 21:18:33 +0300

    Add files via upload

--- Practice 3 modified.py
+++ Practice 3 modified.py
@@ -1,5 +1,5 @@
-quantity_home_work = '12'
-quantity_hours = '1.5'
+quantity_HW = 12
+quantity_hours = 1.5
 course_name = 'python'
-time_for_one_task = float(quantity_hours) / float(quantity_home_work)
-print(course_name, quantity_home_work, quantity_hours, time_for_one_task)
+timeForOneTask = quantity_hours / quantity_HW
+print(course_name, quantity_HW, quantity_hours, timeForOneTask)
