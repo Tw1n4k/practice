8885da75a36497082af5300f65872e5bd0904a9a
Tw1n4k <Krivonosnikolay@mail.ru>
2024-07-24 13:53:50 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,9 @@
+immutable_var = (1, 2, 'banana ', 'engine oil ', 'CPU')
+print(immutable_var)
+#immutable_var.remove('banana ')
+#print(immutable_var)
+# Кортеж - неизменяемый объект.
+mutable_list = [1, 2, 'notebook', 'door', 'apple']
+print(mutable_list)
+mutable_list.extend(['string', 'book'])
+print(mutable_list)
